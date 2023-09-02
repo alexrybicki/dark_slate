6ec6ea31becd3d125540f7c282814b6ac3678271
Alex <alex.rybicki@gmail.com>
2023-09-02 10:50:01 -0700

    Update flip.js

--- assets/JS/flip.js
+++ assets/JS/flip.js
@@ -1,4 +1,14 @@
- var card = document.querySelector('.card');
-        card.addEventListener( 'click', function() {
-        card.classList.toggle('is-flipped');
-      });
+var card = document.querySelector('.card');
+if (card !== undefined && card !== null) {
+  card.addEventListener( 'click', function() {
+  card.classList.toggle('is-flipped');
+ })
+};
+var element = document.getElementById('back-link');
+  if (element !== undefined && element !== null) {
+    element.setAttribute('href', document.referrer);
+    element.onclick = function() {
+      history.back();
+      return false;
+  }
+};
